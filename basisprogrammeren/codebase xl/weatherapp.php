a3152ffd1d5e2fefb4c2ec74a5447fb8aa03731e
leqxi <97111949@st.deltion.nl>
2023-10-04 22:03:25 +0200

    :)

--- basisprogrammeren/codebase xl/weatherapp.php
+++ basisprogrammeren/codebase xl/weatherapp.php
@@ -5,8 +5,25 @@ $latitude = 52.520899;
 $longitude = 6.081883;
 
 $url = "https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid=$api_key";
-?>
 
+//$response = file_get_contents($url);
+
+//if ($response === false) {
+//    die('Failed to fetch data from OpenWeatherMap API');
+}
+
+//$data = json_decode($response, true);
+
+//if ($data === null) {
+    //die('Failed to decode JSON data from OpenWeatherMap API');
+}
+
+// if (isset($data['list'][0])) {
+//    $temperature = $data['list'][0]['main']['temp'];
+} //else {
+//    die('Data from OpenWeatherMap API does not contain the expected structure.');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +32,7 @@ $url = "https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$long
     <title>Weather App</title>
 </head>
 <body>
-    
+    <h1>Het weer voor gebouw Groen</h1>
+    <p>Temperature: <?php echo $temperature; ?> &deg;K</p>
 </body>
 </html>
