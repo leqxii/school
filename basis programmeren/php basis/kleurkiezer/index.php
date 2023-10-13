ca02c9736e80ada356fdb6e55741d1c2c27471c6
Kevin Pierik <97111949@st.deltion.nl>
2023-10-13 18:58:04 +0200

    :ambulance:

--- basis programmeren/php basis/kleurkiezer/index.php
+++ basis programmeren/php basis/kleurkiezer/index.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kleurenkiezer</title>
+    <title>Kleurkiezer</title>
     
     <?php
     if (!empty($selectedColor)) {
