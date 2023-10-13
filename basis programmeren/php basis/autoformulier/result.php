4ccafae8ad6cd4daa40247607db7d72d4210b557
leqxi <97111949@st.deltion.nl>
2023-10-13 22:48:36 +0200

    :ant:

---
+++ basis programmeren/php basis/autoformulier/result.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto formulier</title>
+</head>
+<body>
+    <h1>Nice cars</h1>
+    <?php
+        if (isset($_GET['auto'])) {
+        $selectedCar = $_GET['auto'];
+        echo '<img src="img/' . $selectedCar . '-1.png" alt="' . $selectedCar . ' 1">';
+        echo '<img src="img/' . $selectedCar . '-2.png" alt="' . $selectedCar . ' 2">';
+    }
+    ?>
+</body>
+</html>
