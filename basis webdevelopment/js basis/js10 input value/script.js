89e02fff0cb9242b6d189192fa9819963f75d303
Kevin Pierik <97111949@st.deltion.nl>
2023-10-16 12:34:53 +0200

    :+1:

---
+++ basis webdevelopment/js basis/js10 input value/script.js
@@ -0,0 +1,10 @@
+function genereerOutput() {
+    let voornaam = document.getElementById('voornaam').value;
+    let achternaam = document.getElementById('achternaam').value;
+    let leeftijd = document.getElementById('leeftijd').value;
+    let jaartal = document.getElementById('jaartal').value;
+
+    let nieuweLeeftijd = parseInt(leeftijd) + parseInt(jaartal);
+    let outputdiv = "Hallo ik ben " + voornaam + " " + achternaam + ". Over " + jaartal + " jaar ben ik " + nieuweLeeftijd + " jaar!";
+    document.getElementById("output").innerHTML = outputdiv;
+}
---
+++ basis webdevelopment/js basis/js11 monkey shuffle/script.js
@@ -0,0 +1,25 @@
+var shuffleButton = document.getElementById("shuffleButton");
+
+shuffleButton.addEventListener("click", function() {
+    var images = document.getElementsByTagName("img");
+    var random = 0;
+    var randomNumbers = [];
+
+    while (randomNumbers.length < 9) {
+        random = Math.floor(Math.random() * 9) + 1;
+        if (randomNumbers.indexOf(random) == -1) {
+            randomNumbers.push(random);
+        }
+    }
+
+    random = 0;
+    for (var i = 0; i < images.length; i++) {
+        images[i].src = "img/image" + randomNumbers[random] + ".png";
+        random++;
+    }
+});
+
+
+
+
+
