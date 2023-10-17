a1bb39d400cb79ed91aa00aa5f70b25d6f6b8d30
Kevin Pierik <97111949@st.deltion.nl>
2023-10-17 17:12:23 +0200

    :)

---
+++ basis programmeren/php basis/pizza/resultaat.php
@@ -0,0 +1,17 @@
+<?php
+$totaalPrijs = 0;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Circulare Delicioso Bestelling</title>
+  </head>
+  <body>
+    <h1>Bestelling Overzicht</h1>
+    <h3>Je bestelling:</h3>
+  </body>
+</html>
