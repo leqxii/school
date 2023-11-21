312c9a2b19e95f2004b8a9dbdafe29c0e286c5a2
leqxii <97111949@st.deltion.nl>
2023-11-21 15:43:00 +0100

    :)

--- formatieve toetsen/1/diagnostischetoets.php
+++ formatieve toetsen/1/diagnostischetoets.php
@@ -1,31 +1,61 @@
 <?php
-  $leeftijden = [];
+    $aantalPersonen = 5;
+    $leeftijden = vraagLeeftijden($aantalPersonen);
 
-  /* Vraag voor input. */
-  for ($i = 0; $i < 5; $i++) {
-    do {
-      $leeftijd = (float) readline(($i + 1) . ". Geef leeftijd op: ");
-    } while (!is_numeric($leeftijd));
-    
-    $leeftijden[] = $leeftijd;
-  }
+    // Bereken statistieken
+    list($boven18, $onder18, $gemiddeldeLeeftijd) = berekenStatistieken($leeftijden);
 
-  /* Berekeningen. */
-  $boven18 = 0;
-  $onder18 = 0;
-  $totaalLeeftijd = 0;
+    // Toon resultaten
+    toonResultaten($boven18, $onder18, $gemiddeldeLeeftijd);
+?>
 
-  foreach ($leeftijden as $leeftijd) {
-    if ($leeftijd >= 18) {
-        $boven18++;
-    } else {
-        $onder18++;
+<?php
+    function vraagLeeftijden($aantalPersonen)
+    {
+        $leeftijden = [];
+
+        for ($i = 0; $i < $aantalPersonen; $i++) 
+        {
+            do 
+            {
+                $leeftijd = (float)readline(($i + 1) . ". Geef leeftijd op: ");
+            } 
+            while (!is_numeric($leeftijd));
+
+            $leeftijden[] = $leeftijd;
+        }
+
+        return $leeftijden;
     }
-    $totaalLeeftijd += $leeftijd;
-  }
-  $gemiddeldeLeeftijd = $totaalLeeftijd / count($leeftijden);  
 
-  /* Output. */
-  echo "Er " . ($boven18 == 1 ? "is 1 persoon" : "zijn $boven18 personen") . " 18 jaar of ouder.\n";
-  echo "Er " . ($onder18 == 1 ? "is 1 persoon" : "zijn $onder18 personen") . " jonger dan 18 jaar.\n";
-  echo "De gemiddelde leeftijd is $gemiddeldeLeeftijd jaar.\n";
+    function berekenStatistieken($leeftijden)
+    {
+        $boven18 = 0;
+        $onder18 = 0;
+        $totaalLeeftijd = 0;
+
+        foreach ($leeftijden as $leeftijd) 
+        {
+            if ($leeftijd >= 18) 
+            {
+                $boven18++;
+            } 
+            else 
+            {
+                $onder18++;
+            }
+            $totaalLeeftijd += $leeftijd;
+        }
+
+        $gemiddeldeLeeftijd = $totaalLeeftijd / count($leeftijden);
+
+        return [$boven18, $onder18, $gemiddeldeLeeftijd];
+    }
+
+    function toonResultaten($boven18, $onder18, $gemiddeldeLeeftijd)
+    {
+        echo "Er " . ($boven18 == 1 ? "is 1 persoon" : "zijn $boven18 personen") . " 18 jaar of ouder.\n";
+        echo "Er " . ($onder18 == 1 ? "is 1 persoon" : "zijn $onder18 personen") . " jonger dan 18 jaar.\n";
+        echo "De gemiddelde leeftijd is $gemiddeldeLeeftijd jaar.\n";
+    }
+?>
