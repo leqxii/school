ea34a57d085672af549bdcfc580c3815419abb73
leqxii <kevinpieriknl@gmail.com>
2024-01-18 08:54:31 +0100

    :)

--- basis webdevelopment/php intermediate/cijferapplicatie/index.php
+++ basis webdevelopment/php intermediate/cijferapplicatie/index.php
@@ -6,34 +6,36 @@
   $wachtwoord = "";
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $gebruikersnaam = $_POST["username"];
-      $wachtwoord = $_POST["password"];
+    $gebruikersnaam = $_POST["username"];
+    $wachtwoord = $_POST["password"];
   }
 
   $query_leerling = "SELECT * FROM studenten WHERE login = '$gebruikersnaam' AND password = '$wachtwoord'";
   $result_leerling = $conn->query($query_leerling);
 
   if ($result_leerling->num_rows > 0) {
-      $_SESSION["gebruiker"] = "leerling";
-      $_SESSION["student_id"] = "";
-      header("Location: users/leerling_home.php");
-      exit();
+    while ($row = $result_leerling->fetch_assoc()) {
+      $_SESSION['ID'] = $row['student_id'];
+    }
+    $_SESSION["gebruiker"] = "leerling";
+    header("Location: users/leerling_home.php");
+    exit();
   } elseif ($gebruikersnaam == "Famke" && $wachtwoord == "123") {
-      $_SESSION["gebruiker"] = "docent";
-      header("Location: users/docent_home.php");
-      exit();
+    $_SESSION["gebruiker"] = "docent";
+    header("Location: users/docent_home.php");
+    exit();
   } else {
-      $foutmelding = "Ongeldige gebruikersnaam of wachtwoord.";
+    $foutmelding = "Ongeldige gebruikersnaam of wachtwoord.";
   }
 
   $conn->close();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Login Page</title>
-  <link rel="stylesheet" type="text/css" href="css/index.css">
-</head>
+  <head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  </head>
   <body>
     <div class="form">
       <p>Login</p>
--- basis webdevelopment/php intermediate/cijferapplicatie/users/leerling_home.php
+++ basis webdevelopment/php intermediate/cijferapplicatie/users/leerling_home.php
@@ -6,19 +6,36 @@
     header("Location: ../index.php");
     exit();
   }
+  $student_id = $_SESSION['ID'];
 
-  $student_id = $_SESSION["student_id"];
+  $query_resultaten = "SELECT * FROM resultaten WHERE student_id = '$student_id'";
+  $result_resultaten = $conn->query($query_resultaten);
+
+  $results = $result_resultaten->fetch_all(MYSQLI_ASSOC);
 
   $conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
   <body>
-    <?=$student_id?>
+    <table>
+      <tr>
+        <th>Resultaat ID</th>
+        <th>Toets ID</th>
+        <th>Cijfer</th>
+      </tr>
+      <?php foreach ($results as $result): ?>
+      <tr>
+        <td><?= $result['resultaat_id'] ?></td>
+        <td><?= $result['toets_id'] ?></td>
+        <td><?= $result['cijfer'] ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </table>
   </body>
 </html>
